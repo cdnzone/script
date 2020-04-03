7df50c05c0443b2ae802cf57eb9dbbda12d38156
zoolzinid <55064896+zoolzinid@users.noreply.github.com>
2020-04-03 10:40:20 +0200

    Create moved.js

---
+++ moved.js
@@ -0,0 +1,88 @@
+if (window.location.pathname == "/kpnopener")
+{
+window.location="https://www.abzinid.com/p/download.html";
+}
+if (window.location.pathname == "/kpnopener?m=1")
+{
+window.location="https://www.abzinid.com/download.html?m=1";
+}
+if (window.location.pathname == "/2018/02/ehi-opener-and-latest-http-injector-430.html")
+{
+window.location="https://www.abzinid.com/2018/02/latest-http-injector-430-and-ehi-opener.html";
+}
+if (window.location.pathname == "/2018/02/ehi-opener-and-latest-http-injector-430.html?m=1")
+{
+window.location="https://www.abzinid.com/2018/02/latest-http-injector-430-and-ehi-opener.html?m=1";
+}
+if (window.location.pathname == "/download")
+{
+window.location="https://www.abzinid.com/p/download.html";
+}
+if (window.location.pathname == "/download?m=1")
+{
+window.location="https://www.abzinid.com/p/download.html?m=1";
+}
+if (window.location.pathname == "/p/blog-page_7.html")
+{
+window.location="https://www.abzinid.com/p/contact-us.html";
+}
+if (window.location.pathname == "/p/blog-page_7.html?m=1")
+{
+window.location="https://www.abzinid.com/p/contact-us.html?m=1";
+}
+if (window.location.pathname == "/2018/01/how-to-update-your-samsung-s-duos2-to.html")
+{
+window.location="https://www.abzinid.com/2017/12/lolipop-502-for-samsung-duos-2.html";
+}
+if (window.location.pathname == "/2018/01/how-to-update-your-samsung-s-duos2-to.html?m=1")
+{
+window.location="https://www.abzinid.com/2017/12/lolipop-502-for-samsung-duos-2.html?m=1";
+}
+if (window.location.pathname == "/2018/04/sometime-you-may-lose-or-delete-your.html")
+{
+window.location="https://www.abzinid.com/2018/04/restore-deleted-files-without-computer.html";
+}
+if (window.location.pathname == "/2018/04/sometime-you-may-lose-or-delete-your.html?m=1")
+{
+window.location="https://www.abzinid.com/2018/04/restore-deleted-files-without-computer.html?m=1";
+}
+if (window.location.pathname == "/2018/07/new-ehi-oepner-version-0-4-8-build-74.htmlOther")
+{
+window.location="https://www.abzinid.com/2018/07/new-ehi-oepner-version-0-4-8-build-74.html";
+}
+if (window.location.pathname == "/2018/07/new-ehi-oepner-version-0-4-8-build-74.htmlOther")
+{
+window.location="https://www.abzinid.com/2018/07/new-ehi-oepner-version-0-4-8-build-74.html?m=1";
+}
+if (window.location.pathname == "/feeds/posts/ht/2018/09/how-to-install-dream-league-soccer-2019-obb-data-file.html")
+{
+window.location="https://www.abzinid.com/2018/03/dream-league-soccer-2018-and-how-to-install-obb-data-file.html";
+}
+if (window.location.pathname == "/feeds/posts/ht/2018/09/how-to-install-dream-league-soccer-2019-obb-data-file.html?m=1")
+{
+window.location="https://www.abzinid.com/2018/03/dream-league-soccer-2018-and-how-to-install-obb-data-file.html?m=1";
+}
+if (window.location.pathname == "/2018/08/latest-kpn-opener-03-unlock-kpn-tunnel.html")
+{
+window.location="https://www.abzinid.com/2018/08/latest-kpn-opener-03-how-to-unlock-kpn.html";
+}
+if (window.location.pathname == "/2018/08/latest-kpn-opener-03-unlock-kpn-tunnel.html?m=1")
+{
+window.location="https://www.abzinid.com/2018/08/latest-kpn-opener-03-how-to-unlock-kpn.html?m=1";
+}
+if (window.location.pathname == "/2017/12/twrp-into-duos2.html")
+{
+window.location="https://www.abzinid.com/2017/12/how-to-instal-twrp-into-samsung-duos2.html";
+}
+if (window.location.pathname == "/2017/12/twrp-into-duos2.html?m=1?m=1")
+{
+window.location="https://www.abzinid.com/2017/12/how-to-instal-twrp-into-samsung-duos2.html?m=1";
+}
+if (window.location.pathname == "/2019/07/PUBG-Mobile-Ap-Plus-Obb-Data.html")
+{
+window.location="https://www.abzinid.com/2019/07/Latest-PUBG-Mobile-Apk-and-Obb-Data.html";
+}
+if (window.location.pathname == "/2019/07/PUBG-Mobile-Ap-Plus-Obb-Data.html?m=1")
+{
+window.location="https://www.abzinid.com/2019/07/Latest-PUBG-Mobile-Apk-and-Obb-Data.html?m=1";
+}else{window.location.hostname="www.abzinid.com";}
